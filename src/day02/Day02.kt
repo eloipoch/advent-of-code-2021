4cee9b23f51a95e22ff67aab596616522a02322e
Eloi Poch <eloi.poch@gmail.com>
2021-12-02 17:36:07 +0100

    day two part two

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -6,18 +6,28 @@ import readInput
 fun main() {
     fun part1(input: List<String>) = input
         .map { it.split(" ") }
-        .map { (direction, points) -> direction to points.toInt() }
-        .fold(0 to 0) { (horizontal, depth), (direction, points) ->
+        .map { (direction, units) -> direction to units.toInt() }
+        .fold(0 to 0) { (horizontal, depth), (direction, units) ->
             when (direction) {
-                "forward" -> horizontal + points to depth
-                "down" -> horizontal to depth + points
-                "up" -> horizontal to depth - points
+                "forward" -> horizontal + units to depth
+                "down" -> horizontal to depth + units
+                "up" -> horizontal to depth - units
                 else -> horizontal to depth
             }
         }
         .run { first * second }
 
-    fun part2(input: List<String>) = input.size
+    fun part2(input: List<String>) = input
+        .map { it.split(" ") }
+        .map { (direction, units) -> direction to units.toInt() }
+        .fold(Position.create()) { position, (direction, units) ->
+            when (direction) {
+                "forward" -> position.forward(units)
+                "down" -> position.down(units)
+                "up" -> position.up(units)
+                else -> position
+            }
+        }.result()
 
     val testInput = readInput("day02/Day02_test")
 
@@ -27,4 +37,16 @@ fun main() {
     val input = readInput("day02/Day02")
     printResult("Part 1") { part1(input) }
     printResult("Part 2") { part2(input) }
+}
+
+data class Position(val horizontal: Int, val depth: Int, val aim: Int) {
+    fun down(units: Int) = copy(aim = aim + units)
+    fun up(units: Int) = copy(aim = aim - units)
+    fun forward(units: Int) = Position(horizontal + units, depth + (aim * units), aim)
+
+    fun result() = horizontal * depth
+
+    companion object {
+        fun create() = Position(0, 0, 0)
+    }
 }
