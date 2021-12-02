e236814d8d4595f03e6bf93a822f0ce0f52b54ef
Eloi Poch <eloi.poch@gmail.com>
2021-12-02 10:50:29 +0100

    day one part one

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,11 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Print the result of the block next line after the title
+ */
+fun printResult(title: String, block: () -> Any) {
+    println("Result $title")
+    println(block())
+}
--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,21 +1,22 @@
 package day01
 
+import printResult
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input
+        .map { it.toInt() }
+        .zipWithNext { first, second -> if (first < second) "inc" else "dec" }
+        .count { "inc" == it }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    printResult("Test") { part1(readInput("day01/Day01_test")) }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("day01/Day01")
+
+    printResult("Part 1") { part1(input) }
+    printResult("Part 2") { part2(input) }
 }
