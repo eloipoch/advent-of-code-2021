7018f146c19469cb74303df435f1c569aed36966
Eloi Poch <eloi.poch@gmail.com>
2021-12-06 15:48:01 +0100

    improve day five both parts using kotlin ranges, sealed & iterators

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,13 +1,15 @@
 package day05
 
+import day05.Vector.Horizontal
+import day05.Vector.Vertical
 import printResult
 import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val vents = input.map {
-            it.split(" -> ").map(Point::parse).run { SimpleVector(this[0], this[1]) }
-        }
+        val vents = input.map { it.split(" -> ") }
+            .map { (x, y) -> x.asPoint()..y.asPoint() }
+            .filter { it is Horizontal || it is Vertical }
 
         return vents.flatten()
             .groupingBy { it }
@@ -16,11 +18,9 @@ fun main() {
             .count()
     }
 
-
     fun part2(input: List<String>): Int {
-        val vents = input.map {
-            it.split(" -> ").map(Point::parse).run { ComplexVector(this[0], this[1]) }
-        }
+        val vents = input.map { it.split(" -> ") }
+            .map { (x, y) -> x.asPoint()..y.asPoint() }
 
         return vents.flatten()
             .groupingBy { it }
@@ -38,27 +38,21 @@ fun main() {
     printResult("Part 2") { part2(input) }
 }
 
-data class SimpleVector(private val list: List<Point>) : List<Point> by list {
-    constructor(a: Point, b: Point) : this(points(minOf(a, b), maxOf(a, b)))
+sealed class Vector(from: Point, to: Point) : Iterable<Point>, ClosedRange<Point> {
+    class Horizontal(from: Point, to: Point) : Vector(from, to)
+    class Vertical(from: Point, to: Point) : Vector(from, to)
+    class Diagonal(from: Point, to: Point) : Vector(from, to)
+
+    override val start: Point = minOf(from, to)
+    override val endInclusive: Point = maxOf(from, to)
+
+    override fun iterator(): Iterator<Point> = VectorIterator(this)
 
     companion object {
-        fun points(from: Point, to: Point) = when (true) {
-            from.x == to.x -> (from.y..to.y).map { y -> Point(from.x, y) }
-            from.y == to.y -> (from.x..to.x).map { x -> Point(x, from.y) }
-            else -> emptyList()
-        }
-    }
-}
-
-data class ComplexVector(private val list: List<Point>) : List<Point> by list {
-    constructor(a: Point, b: Point) : this(points(minOf(a, b), maxOf(a, b)))
-
-    companion object {
-        fun points(from: Point, to: Point) = when (true) {
-            from.x == to.x -> (from.y..to.y).map { y -> Point(from.x, y) }
-            from.y == to.y -> (from.x..to.x).map { x -> Point(x, from.y) }
-            from.y < to.y -> (from.x..to.x).mapIndexed() { step, x -> Point(x, from.y + step) }
-            else -> (from.x..to.x).mapIndexed() { step, x -> Point(x, from.y - step) }
+        operator fun invoke(from: Point, to: Point) = when (true) {
+            from.x == to.x -> Horizontal(from, to)
+            from.y == to.y -> Vertical(from, to)
+            else -> Diagonal(from, to)
         }
     }
 }
@@ -68,9 +62,30 @@ data class Point(val x: Int, val y: Int) : Comparable<Point> {
         return if (x != other.x) x - other.x else y - other.y
     }
 
-    companion object {
-        fun parse(string: String) =
-            string.split(",").let { (x, y) -> Point(x.toInt(), y.toInt()) }
+    operator fun rangeTo(other: Point) = Vector(this, other)
+}
+
+class VectorIterator(private val vector: Vector) : Iterator<Point> {
+    private val step: Int = 1
+    private var current = vector.start
+
+    override fun hasNext() = current <= vector.endInclusive
+
+    override fun next(): Point {
+        val next = current
+        current = current.next()
+        return next
+    }
+
+    private fun Point.next() = when (vector) {
+        is Horizontal -> Point(x, y + step)
+        is Vertical -> Point(x + step, y)
+        is Vector.Diagonal -> when (vector.start.y < vector.endInclusive.y) {
+            true -> Point(x + step, y + step)
+            false -> Point(x + step, y - step)
+        }
     }
 }
+
+private fun String.asPoint() = split(",").let { (x, y) -> Point(x.toInt(), y.toInt()) }
 
