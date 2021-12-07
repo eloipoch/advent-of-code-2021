98946f42703d4e8f94ed4c5bddb2099116bee686
Eloi Poch <eloi.poch@gmail.com>
2021-12-07 18:53:52 +0100

    day seven part two

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -1,15 +1,16 @@
 package day07
 
 import day07.Day07.part1
+import day07.Day07.part2
 import printResult
 import readInput
 import toInts
-import kotlin.Long.Companion.MAX_VALUE
+import kotlin.Int.Companion.MAX_VALUE
 import kotlin.math.abs
 import kotlin.math.min
 
 object Day07 {
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Int {
         val crabs = input.first().split(",").toInts()
         val min = crabs.minOf { it }
         val max = crabs.maxOf { it }
@@ -17,22 +18,33 @@ object Day07 {
         return (min..max).fold(MAX_VALUE) { best, current ->
             min(
                 best,
-                crabs.fold(0L) { movements, crab -> movements + abs(crab - current) }
+                crabs.fold(0) { movements, crab -> movements + abs(crab - current) }
             )
         }
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val crabs = input.first().split(",").toInts()
+        val min = crabs.minOf { it }
+        val max = crabs.maxOf { it }
+
+        return (min..max).fold(MAX_VALUE) { best, current ->
+            min(
+                best,
+                crabs.fold(0) { movements, crab -> movements + cost(abs(crab - current)) }
+            )
+        }
     }
+
+    private fun cost(n: Int): Int = if (n <= 1) 1 else n + cost(n - 1)
 }
 
 fun main() {
     val testInput = readInput("day07/Day07_test")
-    with(part1(testInput)) { check(37L == this) { "result test 1: $this" } }
-//    with(part2(testInput)) { check(? == this) { "result test 2: $this" } }
+    with(part1(testInput)) { check(37 == this) { "result test 1: $this" } }
+    with(part2(testInput)) { check(168 == this) { "result test 2: $this" } }
 
     val input = readInput("day07/Day07")
     printResult("Part 1") { part1(input) }
-//    printResult("Part 2") { part2(input) }
+    printResult("Part 2") { part2(input) }
 }
