f76cb376caf490191ffc18291e4ee70551ed9faa
Eloi Poch <eloi.poch@gmail.com>
2021-12-07 13:58:22 +0100

    template for every day

---
+++ src/dayXX/DayXX.kt
@@ -0,0 +1,23 @@
+package dayXX
+
+import readInput
+
+object DayXX {
+    fun part1(input: List<String>): Int {
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+}
+
+fun main() {
+    val testInput = readInput("dayXX/DayXX_test")
+//    with(part1(testInput)) { check(? == this) { "result test 1: $this" } }
+//    with(part2(testInput)) { check(? == this) { "result test 2: $this" } }
+
+    val input = readInput("dayXX/DayXX")
+//    printResult("Part 1") { part1(input) }
+//    printResult("Part 2") { part2(input) }
+}
