b198399ac3bc987b05626fa9844bbdd3a2487271
Eloi Poch <eloi.poch@gmail.com>
2021-12-09 10:52:36 +0100

    improve day eight both parts removing unnecessary folds

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -11,40 +11,37 @@ object Day08 {
     fun part1(input: List<String>) = input.map { it.split(" | ") }
         .map { (_, output) -> output.split(" ") }
         .flatten()
-        .fold(0) { amount, digit -> if (digit.isDesiredDigit()) amount + 1 else amount }
-
-    private fun String.isDesiredDigit() = count() in listOf(2, 3, 4, 7)
+        .count { it.count() in listOf(2, 3, 4, 7) }
 
     fun part2(input: List<String>): Int {
-        return input.fold(0) { result, display ->
+        return input.sumOf { display ->
             val (pattern, output) = display.split(" | ")
             val patterns = pattern.asSignal()
             val outputs = output.asSignal()
 
-            val one = patterns.first { it.count() == 2 }
-            val four = patterns.first { it.count() == 4 }
+            val digitOne = patterns.first { it.count() == 2 }
+            val digitFour = patterns.first { it.count() == 4 }
 
-            val codes = patterns
-                .associateWith { digit ->
-                    when (digit.size) {
-                        2 -> 1
-                        3 -> 7
-                        4 -> 4
-                        7 -> 8
-                        5 -> when {
-                            digit.intersect(four).size == 2 -> 2
-                            digit.intersect(one).size == 2 -> 3
-                            else -> 5
-                        }
-                        else -> when { //6
-                            digit.intersect(one).size == 1 -> 6
-                            digit.intersect(four).size == 4 -> 9
-                            else -> 0
-                        }
+            val codes = patterns.associateWith { digit ->
+                when (digit.size) {
+                    2 -> 1
+                    3 -> 7
+                    4 -> 4
+                    7 -> 8
+                    5 -> when {
+                        digit.intersect(digitFour).size == 2 -> 2
+                        digit.intersect(digitOne).size == 2 -> 3
+                        else -> 5
+                    }
+                    else -> when { //6
+                        digit.intersect(digitOne).size == 1 -> 6
+                        digit.intersect(digitFour).size == 4 -> 9
+                        else -> 0
                     }
                 }
+            }
 
-            result + outputs.map { codes[it]!! }.joinToString("").toInt()
+            outputs.map { codes[it]!! }.joinToString("").toInt()
         }
     }
 
@@ -57,6 +54,6 @@ fun main() {
     with(part2(testInput)) { check(61229 == this) { "result test 2: $this" } }
 
     val input = readInput("day08/Day08")
-    printResult("Part 1") { part1(input) }
+    printResult("Part 1") { part1(input) } //539
     printResult("Part 2") { part2(input) }
 }
