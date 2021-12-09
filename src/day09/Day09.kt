acacd1ada329b98428fa58f50e1c0bcf865ae3cd
Eloi Poch <eloi.poch@gmail.com>
2021-12-09 23:40:20 +0100

    day nine part two

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -1,6 +1,7 @@
 package day09
 
 import day09.Day09.part1
+import day09.Day09.part2
 import day09.Point.Position
 import printResult
 import readInput
@@ -8,18 +9,20 @@ import readInput
 typealias HeightMap = Array<Array<Point>>
 
 object Day09 {
-    fun part1(input: List<String>): Int {
-        val points: HeightMap = input.mapIndexed { y, line ->
-            line.mapIndexed { x, digit ->
-                Point(x, y, digit.digitToInt())
-            }.toTypedArray()
-        }.toTypedArray()
+    fun part1(input: List<String>) = heightMap(input).lowPoints().sumOf { it.value + 1 }
 
-        return points.lowPoints().sumOf { it.value + 1 }
+    fun part2(input: List<String>): Int {
+        val heightMap = heightMap(input)
+
+        return heightMap.lowPoints()
+            .map { heightMap.basinOf(it).count() }
+            .sortedDescending()
+            .take(3)
+            .reduce { result, basin -> result * basin }
     }
 
     private fun HeightMap.lowPoints() =
-        flatten().filter { it.value < adjacent(it).minOf(Point::value) }
+        flatten().filter { point -> point < adjacent(point).minOf { it } }
 
     private fun HeightMap.adjacent(point: Point) =
         listOf(point.position.top(), point.position.right(), point.position.bottom(), point.position.left())
@@ -27,12 +30,24 @@ object Day09 {
 
     private fun HeightMap.pointOf(position: Position) = getOrNull(position.y)?.getOrNull(position.x)
 
-    fun part2(input: List<String>): Int {
-        return 1
+    private fun heightMap(input: List<String>): HeightMap = input.mapIndexed { y, line ->
+        line.mapIndexed { x, digit -> Point(x, y, digit.digitToInt()) }.toTypedArray()
+    }.toTypedArray()
+
+    private fun HeightMap.basinOf(point: Point): Set<Point> {
+        val moreBasin = adjacent(point)
+            .filterNot { it.value == 9 }
+            .filter { it > point }
+
+        return if (moreBasin.isEmpty()) {
+            setOf(point)
+        } else {
+            moreBasin.fold(setOf(point)) { basin, another -> basin + basinOf(another) }
+        }
     }
 }
 
-data class Point(val position: Position, val value: Int) {
+data class Point(val position: Position, val value: Int) : Comparable<Point> {
     constructor(x: Int, y: Int, value: Int) : this(Position(x, y), value)
 
     data class Position(val x: Int, val y: Int) {
@@ -41,14 +56,16 @@ data class Point(val position: Position, val value: Int) {
         fun bottom() = Position(x, y + 1)
         fun left() = Position(x - 1, y)
     }
+
+    override fun compareTo(other: Point) = value.compareTo(other.value)
 }
 
 fun main() {
     val testInput = readInput("day09/Day09_test")
     with(part1(testInput)) { check(15 == this) { "result test 1: $this" } }
-//    with(part2(testInput)) { check(? == this) { "result test 2: $this" } }
+    with(part2(testInput)) { check(1134 == this) { "result test 2: $this" } }
 
     val input = readInput("day09/Day09")
     printResult("Part 1") { part1(input) }
-//    printResult("Part 2") { part2(input) }
+    printResult("Part 2") { part2(input) }
 }
