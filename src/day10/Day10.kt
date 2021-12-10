13ba5d20c058037c6a6e81a31c7a55d1270cd56f
Eloi Poch <eloi.poch@gmail.com>
2021-12-10 11:23:14 +0100

    day ten part two

--- src/day10/Day10.kt
+++ src/day10/Day10.kt
@@ -1,6 +1,7 @@
 package day10
 
 import day10.Day10.part1
+import day10.Day10.part2
 import printResult
 import readInput
 
@@ -11,9 +12,30 @@ object Day10 {
             "]".single() -> 57
             "}".single() -> 1197
             ">".single() -> 25137
-            else -> 0 //remove not possible caseπ
+            else -> 0 //remove not possible case
         }
     }.sum()
+
+    fun part2(input: List<String>): Long {
+        val test = input.map(String::toCharArray).filter { corrupted(it) == null }
+            .map(::opened)
+            .map { it.map(::expectedClosing).reversed() }
+            .map {
+                it.fold(0L) { total, char ->
+                    val value = when (char) {
+                        ")".single() -> 1
+                        "]".single() -> 2
+                        "}".single() -> 3
+                        ">".single() -> 4
+                        else -> 0 //remove not possible case
+                    }
+
+                    ((total * 5) + value).toLong()
+                }
+            }
+
+        return test.sorted()[test.size / 2]
+    }
 
     private fun corrupted(line: CharArray, opened: CharArray = charArrayOf()): Char? {
         val current = line.firstOrNull() ?: return null
@@ -29,6 +51,20 @@ object Day10 {
         }
     }
 
+    private fun opened(line: CharArray, opened: CharArray = charArrayOf()): CharArray {
+        val current = line.firstOrNull() ?: return opened
+
+        return if (opening(current)) {
+            opened(line.drop(1).toCharArray(), opened + current)
+        } else {
+            if (current == expectedClosing(opened.last())) {
+                opened(line.drop(1).toCharArray(), opened.dropLast(1).toCharArray())
+            } else {
+                opened
+            }
+        }
+    }
+
     private fun expectedClosing(opening: Char) = when (opening) {
         "(".single() -> ")".single()
         "[".single() -> "]".single()
@@ -39,18 +75,14 @@ object Day10 {
 
     private fun opening(char: Char) = (char in listOf("(", "[", "{", "<").map(String::single))
     private fun closing(char: Char) = opening(char).not()
-
-    fun part2(input: List<String>): Int {
-        return 1
-    }
 }
 
 fun main() {
     val testInput = readInput("day10/Day10_test")
     with(part1(testInput)) { check(26397 == this) { "result test 1: $this" } }
-//    with(part2(testInput)) { check(? == this) { "result test 2: $this" } }
+    with(part2(testInput)) { check(288957L == this) { "result test 2: $this" } }
 
     val input = readInput("day10/Day10")
     printResult("Part 1") { part1(input) }
-//    printResult("Part 2") { part2(input) }
+    printResult("Part 2") { part2(input) }
 }
